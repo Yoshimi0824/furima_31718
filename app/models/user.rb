5ab10fedf4dc3523b5c9ca4b9fee636c1e33a4bb
Yoshimi0824 <yoshi.gaze.0824@gmail.com>
2020-11-13 19:56:31 +0900

    新規登録、ログイン機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,33 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
+  validates :nickname, presence: true
+  validates :birthday, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カナ文字を使用してください' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+  
+  has_many :items
+  has_many :purchases
+  
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
+
+
+#パスワードは、半角英数字混合での入力が必須であること
+#メールアドレスは、@を含む必要があること
+
+#ユーザー本名は、全角（漢字・ひらがな・カタカナ）での入力が必須であること
+#ユーザー本名のフリガナは、全角（カタカナ）での入力が必須であること
