36cdced4641bebadfbc47581ef7ff575c26a9c3a
Yoshimi0824 <yoshi.gaze.0824@gmail.com>
2020-11-18 16:44:42 +0900

    商品一覧表示機能

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -64,9 +64,9 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
       it '価格の範囲が、10,000,000以上では登録できないこと' do
-        @item.price = 10000000
+        @item.price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
       it '販売価格は半角数字のみ保存可能であること' do
         @item.price = '１０００'
