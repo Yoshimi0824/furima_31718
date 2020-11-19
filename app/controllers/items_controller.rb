6b7f141727b70893360bf28464b0b8886eb924c8
Yoshimi0824 <yoshi.gaze.0824@gmail.com>
2020-11-19 15:08:02 +0900

    再修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,8 +39,6 @@ class ItemsController < ApplicationController
   def destroy
     if @item.user_id == current_user.id
       @item.destroy
-    end
-    if @item.destroy 
       redirect_to root_path
     end
   end
