36b0d52f26654cf047c44d620cf82a851f03d775
Yoshimi0824 <yoshi.gaze.0824@gmail.com>
2020-11-21 23:32:56 +0900

    カード決済機能

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,20 +1,19 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :sender, :municipality, :street_number, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :sender, :municipality, :street_number, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
-    #validates :token
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :token
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :sender, numericality: { other_than: 1, message: 'Select' }
     validates :municipality
     validates :street_number
-    validates :phone_number, format: { with: /\A[0-9]+\z/, message: "is invalid. Except hyphen(-)" },
+    validates :phone_number, format: { with: /\A[0-9]+\z/, message: 'is invalid. Except hyphen(-)' },
                              length: { maximum: 11 }
   end
-  
+
   def save
-    purchase = Purchase.create( user_id: user_id, item_id: item_id)
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, prefecture_id: sender, municipality: municipality, street_number: street_number, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
   end
-
 end
--- spec/factories/purchase_addresses.rb
+++ spec/factories/purchase_addresses.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :purchase_address do
-    postal_code   { "111-1111" }
-    sender     { Faker::Number.between(from: 2, to: 48) }
+    postal_code { '111-1111' }
+    sender { Faker::Number.between(from: 2, to: 48) }
     municipality  { Gimei.address.city.kanji }
     street_number { Gimei.address.town.kanji }
-    building_name { "マンション名" }
+    building_name { 'マンション名' }
     phone_number  { Faker::Number.number(digits: 11) }
-    
+    token { 'tok_abcdefghijk00000000000000000' }
   end
 end
