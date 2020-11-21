26ed9a683bb044a3236d4a4ffe6bf2373606904b
Yoshimi0824 <yoshi.gaze.0824@gmail.com>
2020-11-21 16:36:37 +0900

    購入機能単体テストコード

--- spec/factories/purchase_addresses.rb
+++ spec/factories/purchase_addresses.rb
@@ -1,12 +1,11 @@
 FactoryBot.define do
   factory :purchase_address do
-    postal_code
-    prefecture_id
-    municipality
-    street_number
-    building_name
-    phone_number
-    association :user, :item, :purchase
+    postal_code   { "111-1111" }
+    sender     { Faker::Number.between(from: 2, to: 48) }
+    municipality  { Gimei.address.city.kanji }
+    street_number { Gimei.address.town.kanji }
+    building_name { "マンション名" }
+    phone_number  { Faker::Number.number(digits: 11) }
     
   end
 end
--- spec/models/purchase_address_spec.rb
+++ spec/models/purchase_address_spec.rb
@@ -12,5 +12,45 @@ RSpec.describe PurchaseAddress, type: :model do
       end
     end
 
-    
+    context '購入がうまくいかないとき' do
+      it '郵便番号が必須であること' do
+        @purchase_address.postal_code = " "
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it '都道府県が必須であること' do
+        @purchase_address.sender = 1
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Sender Select")
+      end
+      it '市区町村が必須であること' do
+        @purchase_address.municipality = " "
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Municipality can't be blank")
+      end
+      it '番地が必須であること' do
+        @purchase_address.street_number = " "
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Street number can't be blank")
+      end
+      it '電話番号が必須であること' do
+        @purchase_address.phone_number = " "
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it '郵便番号にはハイフンが必要であること(123-4567となる)' do
+        @purchase_address.postal_code = "1234567"
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+      end
+      it '電話番号にはハイフンは不要で、11桁以内であること（09012345678となる）' do
+        @purchase_address.phone_number = "090-1234-5678"
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Phone number is invalid. Except hyphen(-)")
+      end
+
+    end
+  end
+
+
 end
